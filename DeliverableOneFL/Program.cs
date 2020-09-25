4670465df27c5b4929d631be7d8c8024fc516da0
akii <akiichris01@gmail.com>
2020-09-25 02:40:36 -0400

    first commit

---
+++ DeliverableOneFL/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+namespace DeliverableOneFL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int minLength = 7;
+            int maxLength = 12;
+
+            Console.WriteLine("Enter a password:");
+            string userInput = Console.ReadLine();
+            int passwordLength = userInput.Length;
+
+            if (passwordLength >= minLength && passwordLength <= maxLength && userInput.Contains("!") && userInput.Any(char.IsUpper)
+                && userInput.Any(char.IsLower) && userInput.Any(char.IsDigit))
+            {
+
+                Console.WriteLine("Password Valid And Accepted.");
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
+
+        }
+    }
+}
